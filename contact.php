b92362cfb6ca119b967397258510dcdf1a57035b
Unknown <nicolas.orlandini@outlook.fr>
2017-11-17 13:39:03 +0100

    Ajout de la page d'accueil + header + footer
    Contenu a finaliser
    Responsive OK
    Page contact a integrer

--- contact.php
+++ contact.php
@@ -4,44 +4,34 @@ $siteKey = '6LftGzgUAAAAAPIVLFDzuG0nL1e_054V-AltFU3T'; // votre clé publique
 $secret = '6LftGzgUAAAAANgO8xSCE1avLBgJQKUAJPzJjPzb'; // votre clé privée
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="A portfolio template that uses Material Design Lite.">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <title>MDL-Static Website</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">>
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-pink.min.css" />
-    <link rel="stylesheet" href="styles.css" />
+    <meta name="description" content="Formulaire de contact">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./mdl/material.min.css">
+    <link rel="stylesheet" href="./css/contact.css" />
+    <link rel="stylesheet" href="./css/app.css" />
+    <link rel='stylesheet prefetch' href='http:////netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+    <script src="./mdl/material.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <script src="app.js"></script>
+    <script src="./js/contact.js"></script>
+    <title>Contact</title>
 </head>
 
 <body>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <header class="mdl-layout__header mdl-layout__header--waterfall portfolio-header">
-        </header>
-        <main class="mdl-layout__content">
-           
             <div class="mdl-grid portfolio-max-width portfolio-contact">
                 <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
                     <div class="mdl-card__title">
                         <h2 class="mdl-card__title-text">Contactez-moi</h2>
                     </div>
-                    <div class="mdl-card__media">
-                        <img class="article-image" src=" images/contact-image.jpg" border="0" alt="">
-                    </div>
                     <div class="mdl-card__supporting-text">
-                        <p>
-                            J'aime le saucisson    
-                        </p>
-                        <p>
-                            Surtout quand il est bon !
-                        </p>
                         <form action="#" class="" method="POST">
                             <div class="mdl-selectfield mdl-js-selectfield">
                                 <select id="myselect" name="myselect" class="mdl-selectfield__select">
@@ -49,7 +39,7 @@ $secret = '6LftGzgUAAAAANgO8xSCE1avLBgJQKUAJPzJjPzb'; // votre clé privée
                                     <option value="option0_value">Madame</option>
                                     <option value="option1_value">Monsieur</option>
                                 </select>
-                            <label class="mdl-selectfield__label" for="myselect">Civilité</label>
+                                <label class="mdl-selectfield__label" for="myselect">Civilité</label>
                             </div>
                             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" pattern="[A-Z,a-z, ]*" type="text" id="Name">
@@ -84,27 +74,28 @@ $secret = '6LftGzgUAAAAANgO8xSCE1avLBgJQKUAJPzJjPzb'; // votre clé privée
                                 <textarea class="mdl-textfield__input" type="text" rows="5" id="note"></textarea>
                                 <label class="mdl-textfield__label" for="note">Parlez-moi de votre projet...</label>
                             </div>
-                            <?php require_once 'getCaptcha.php'; ?><br><br>
                             <div class="mdl-textfield mdl-js-textfield mdl-textfield--file">
-                                <input class="mdl-textfield__input" placeholder="No file chosen" type="text" id="TEXT_ID" readonly />
+                                <input class="mdl-textfield__input" placeholder="Importez des documents" type="text" id="TEXT_ID" readonly />
                                 <div class="mdl-button mdl-button--icon mdl-button--file">
                                     <i class="material-icons">attach_file</i>
                                     <input type="file" name="NAME" id="ID" onchange="document.getElementById('TEXT_ID').value=this.files[0].name;" />
                                 </div>
                             </div>
-                            <br><br>
+                            <br>
+							<br>
+                            <?php require_once 'getCaptcha.php'; ?>
+							<br>
+							<br>
                             <form action="test-captcha.php" method="POST">
                                 <div class="g-recaptcha" data-sitekey="<?php echo $siteKey; ?>"></div>
                                 <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent btnSubmit" type="submit">
-                                    ENVOYER PEDE
+                                    ENVOYER
                                 </button>
                             </form>
                         </form>
                     </div>
                 </div>
             </div>
-        </main>
-    </div>
     <script src="https://www.google.com/recaptcha/api.js"></script>
 </body>
 
---
+++ js/index.js
@@ -0,0 +1,171 @@
+
+window.onload = function () {
+    var andr = document.getElementById("pieChartAndroid");
+    var ios = document.getElementById("pieChartiOS");
+    var cSharp = document.getElementById("pieChartCSharp");
+    var java = document.getElementById("pieChartJava");
+    var htmlCss = document.getElementById("pieChartHtmlCss");
+
+    var legend = {
+        display: false
+    }
+
+    var options = { 
+        responsive: false,
+        maintainAspectRatio: false,
+        legend: legend,
+        tooltips: {
+            callbacks: {
+               label: function(tooltipItem) {
+                      return tooltipItem.yLabel;
+               }
+            }
+        },
+        scaleShowLabels : false
+    }
+
+    var androidDoughnutChart = new Chart(andr, {
+        type: 'doughnut',
+        data: {
+            labels: ["Android"],
+            datasets: [{
+                label: 'Android',
+                data: [85, 15],
+                backgroundColor: [
+                    'rgba(106, 179, 68, 1)',
+                    'rgba(106, 179, 68, 0.1)'
+                ],
+                borderColor: [
+                    'rgba(106, 179, 68, 1)',
+                    'rgba(106, 179, 68, 0.1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: options
+
+    });
+
+    var iOSDoughnutChart = new Chart(ios, {
+        type: 'doughnut',
+        data: {
+            labels: ["iOS"],
+            datasets: [{
+                label: 'iOS',
+                data: [35, 65],
+                backgroundColor: [
+                    'rgba(51, 51, 51, 1)',
+                    'rgba(51, 51, 51, 0.1)'
+                ],
+                borderColor: [
+                    'rgba(51, 51, 51, 1)',
+                    'rgba(51, 51, 51, 0.1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+
+        options: options,
+    });
+
+    var iOSDoughnutChart = new Chart(cSharp, {
+        type: 'doughnut',
+        data: {
+            labels: ["C#"],
+            datasets: [{
+                label: 'C#',
+                data: [85, 15],
+                backgroundColor: [
+                    'rgba(0, 103, 183, 1)',
+                    'rgba(0, 103, 183, 0.1)'
+                ],
+                borderColor: [
+                    'rgba(0, 103, 183, 1)',
+                    'rgba(0, 103, 183, 0.1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+
+        options: options,
+    });
+
+    var iOSDoughnutChart = new Chart(java, {
+        type: 'doughnut',
+        data: {
+            labels: ["JAVA"],
+            datasets: [{
+                label: 'JAVA',
+                data: [85, 15],
+                backgroundColor: [
+                    'rgba(255, 0, 0, 1)',
+                    'rgba(255, 0, 0, 0.1)'
+                ],
+                borderColor: [
+                    'rgba(255, 0, 0, 1)',
+                    'rgba(255, 0, 0, 0.1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+
+        options: options,
+    });
+
+    var iOSDoughnutChart = new Chart(htmlCss, {
+        type: 'doughnut',
+        data: {
+            labels: ["HTML5 / CSS3"],
+            datasets: [{
+                label: 'HTML5 / CSS3',
+                data: [25, 75],
+                backgroundColor: [
+                    'rgba(255, 102, 0, 1)',
+                    'rgba(255, 102, 0, 0.1)'
+                ],
+                borderColor: [
+                    'rgba(255, 102, 0, 1)',
+                    'rgba(255, 102, 0, 0.1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+
+        options: options,
+    });
+    
+    $("a[href^='#']").click(function (e) {
+		var 
+			yPos,
+			yInitPos,
+			target = ($($(this).attr("href") + ":first"));
+			
+		// On annule le comportement initial au cas ou la base soit différente de la page courante.
+		e.preventDefault(); 
+		
+		yInitPos = $(window).scrollTop();
+		
+		// On ajoute le hash dans l'url.
+		window.location.hash = $(this).attr("href");
+		
+		// Comme il est possible que l'ajout du hash perturbe le défilement, on va forcer le scrollTop à son endroit inital.
+		$(window).scrollTop(yInitPos);
+		
+		// On cible manuellement l'ancre pour en extraire sa position.
+		// Si c'est un ID on l'obtient.
+		target = ($($(this).attr("href") + ":first"));
+ 
+		// Sinon on cherche l'ancre dans le name d'un a.
+		if (target.length == 0) {
+			target = ($("a[name=" + $(this).attr("href").replace(/#/gi,"") + "]:first"))
+		}
+		
+		// Si on a trouvé un name ou un id, on défile.
+		if (target.length == 1) {
+			yPos = target.offset().top; // Position de l'ancre.
+		
+			// On anime le défilement jusqu'à l'ancre.
+			$('html,body').animate({ scrollTop: yPos - 100 }, 600); // On décale de 40 pixels l'affichage pour ne pas coller le bord haut de l'affichage du navigateur et on défile en 1 seconde jusqu'à l'ancre.
+		}
+	});
+}
