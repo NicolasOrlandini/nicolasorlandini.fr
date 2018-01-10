06111e5634a7a56aa5f1dc0d85aa1ae81ab983e4
Unknown <valentin.leon0@gmail.com>
2018-01-11 00:20:50 +0100

    Suppression fichiers et modifications CSS
    Suppression des fichiers du compteur en php et le code dans index.php.
    Modification du parallax pour les ampoules, mais doit avoir une correction sur l'image pour qu'elle soit adaptée pour le mobile.

--- table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `compteVisite` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `date_visite` DATE NOT NULL,
-    `pages_vues` SMALLINT(5) UNSIGNED NOT NULL,
-    PRIMARY KEY (id)
-)
-COLLATE='utf8_general_ci'
-ENGINE=MyISAM;
